9c8165a7d3e2598c55fbaaad0de033e6793203d8
Jay Christy <fortehlose@gmail.com>
2023-12-08 13:15:37 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// Bevy code commonly triggers these lints and they may be important signals
+// about code quality. They are sometimes hard to avoid though, and the CI
+// workflow treats them as errors, so this allows them throughout the project.
+// Feel free to delete this line.
+
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("playerShip1_orange.png"),
+        ..Default::default()
+    });
+}
